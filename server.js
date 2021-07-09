9acdb5e5b76ed97fe6915297d06edd3f42c94687
Gabriel Delaneza <gabrieldelaneza@gmail.com>
2021-07-09 20:36:51 -0300

    server updated, inserted user routes

---
+++ server.js
@@ -0,0 +1,26 @@
+const port = 3000
+
+const express = require('express')
+const server = express()
+
+const productRoutes = require('./src/products/routes')
+const userRoutes = require('./src/users/routes')
+
+const errorHandler = require('./src/middleware/error.handler')
+
+server.use(express.json())
+server.use('/products', productRoutes)
+server.use('/users', userRoutes)
+
+
+// error handling with middleware
+server.use(errorHandler)
+
+
+// server
+server.listen(port, () => {
+    console.log(`Server started at port ${port}`)
+})
+
+
+
