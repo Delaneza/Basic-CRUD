866e4f9c87d8156f93ecf589a40c71464753a505
Rodrigo Pereira <rodrigorpogo@gmail.com>
2021-07-11 19:15:31 -0300

    Adding morgan and dotenv

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const port = 3000
+require('dotenv').config({})
+
+const port = process.env.PORT
 
 const express = require('express')
 const server = express()
+
+const morgan = require('morgan')
 
 const productRoutes = require('./src/products/routes')
 const userRoutes = require('./src/users/routes')
 
 const errorHandler = require('./src/middleware/error.handler')
 
+server.use(morgan('dev'))
 server.use(express.json())
 server.use('/products', productRoutes)
 server.use('/users', userRoutes)
